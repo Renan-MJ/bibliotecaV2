9f83e829e3e069bc8590f2dec8ba33b1743cc776
Renan Januário <RenanMachadoJanuario123@outlook.com>
2026-01-21 22:57:39 -0300

    estrutura frontend

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Biblioteca V2</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Bem-vindo à Biblioteca</h1>
+
+    <ul>
+        <li><a href="livros/listar.php">Livros</a></li>
+        <li><a href="leitores/listar.php">Leitores</a></li>
+        <li><a href="emprestimos/listar.php">Empréstimos</a></li>
+    </ul>
+</body>
+</html>
